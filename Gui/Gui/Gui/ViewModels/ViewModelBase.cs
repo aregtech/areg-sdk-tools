101695ae952fad48b60b2272aa63fbf5f00fce60
Mark Horowitz <mark.horowitz@gmx.net>
2022-12-28 16:05:11 +0100

    Added Multi-Platform Solution 'Gui'

---
+++ Gui/Gui/Gui/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Gui.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Gui/Gui/Gui/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Gui.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
