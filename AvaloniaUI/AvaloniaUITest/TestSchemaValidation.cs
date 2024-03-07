3c5bcaede7f61eb11798f65dd5ad146867f9f03d
Mark Horowitz <28399748+majestikgit@users.noreply.github.com>
2024-03-07 20:19:42 +0100

    Used Liquid Technologies to generate C# model code, in to separate project

--- AvaloniaUI/AvaloniaUITest/TestSchemaValidation.cs
+++ AvaloniaUI/AvaloniaUITest/TestSchemaValidation.cs
@@ -3,13 +3,22 @@ using System.Xml;
 using System.Xml.Linq;
 using System.Xml.Schema;
 using FluentAssertions;
+using Liquid_Technologies;
+using LiquidTechnologies.XmlObjects;
+using Liquid_Technologies.Ns;
 
 namespace AvaloniaUITest;
 
 [TestClass]
 public class TestSchemaValidation
 {
+    private const string ElementDoesntExist = @"Doesn't exist!";
     private static int _numErrorsFound;
+
+    private readonly string _localPath =
+        new Uri(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!).LocalPath;
+
+    private string TestFilesPath => $@"{_localPath}..\..\..\..\TestFiles";
 
     [TestInitialize]
     public void TestInitialize()
@@ -20,10 +29,15 @@ public class TestSchemaValidation
     [TestCleanup]
     public void TestCleanup()
     {
-        Console.WriteLine($"{Environment.NewLine}Number of errors found in the .SIML file = {_numErrorsFound}");
+        Console.WriteLine(@$"{Environment.NewLine}Number of errors found in the .SIML file = {_numErrorsFound}");
+
+        _numErrorsFound.Should().Be(0, "We don't want any validation errors.");
     }
 
     /// <summary>
+    /// The standard Microsoft way of doing things.
+    /// This validates XML files against XSD schemas.
+    /// 
     /// Tests "Aregtech.xsd", which was created with Liquid Technologies Online Tools, then edited.
     /// Source was "Sample.siml".
     /// 
@@ -37,7 +51,102 @@ public class TestSchemaValidation
     [DataRow("Artak_Sample_March_04.xsd", "Sample.siml")]
     public void TestValidateServiceViaSchema(string schemaFileName, string serviceFileName)
     {
-        // Set the validation settings.
+        var xmlReaderSettings = CreateXmlReaderSettings();
+
+        var schema = new XmlSchemaSet();
+        schema.Add("", $@"{TestFilesPath}\{schemaFileName}");
+        var xmlReader = XmlReader.Create($@"{TestFilesPath}\{serviceFileName}", xmlReaderSettings);
+
+        const LoadOptions loadOptions = LoadOptions.PreserveWhitespace | LoadOptions.SetLineInfo;
+        var xmlDocument = XDocument.Load(xmlReader, loadOptions);
+
+        xmlDocument.Validate(schema, ValidationEventHandler);
+    }
+
+    /// <summary>
+    /// This tests using the generated code from <see href="https://www.liquid-technologies.com/xml-objects"/>.
+    /// XSD "Artak_Sample_March_04.xsd" is used, as code was generated against this schema.
+    /// See <see cref="CopiedArtakSampleMarch04Validator"/>.
+    /// See code at <see href="https://www.liquid-technologies.com/Reference/XmlDataBinding/xml-objects-code-xsd-validation.html"/>.
+    /// </summary>
+    [TestMethod]
+    [DataRow("MarksServiceTest.siml")]
+    public void TestLiquidGenCodeFor_Artak_Sample_March_04_Validation_1(string serviceFileName)
+    {
+        var serviceFileFullPath = $@"{TestFilesPath}\{serviceFileName}";
+
+        var validator = new CopiedArtakSampleMarch04Validator();
+        using var validatingReader = validator.CreateValidatingReader(serviceFileFullPath, ValidationEventHandler);
+
+        var serializer = new LxSerializer<ServiceInterfaceElm>();
+        var lxReaderSettings = new LxReaderSettings
+        {
+            ErrorHandler = LxErrorHandler
+        };
+
+        var serviceInterfaceElm = serializer.Deserialize(validatingReader, lxReaderSettings);
+
+        // Iterate through the xml file, and print out data
+        PrintServiceInterface(serviceInterfaceElm);
+    }
+
+    private static void PrintServiceInterface(ServiceInterfaceElm root)
+    {
+        Console.WriteLine(@"Service Interface:");
+        PrintOverview(root.Overview);
+        PrintDataTypeList(root.DataTypeList);
+    }
+
+    private static void PrintOverview(ServiceInterfaceElm.OverviewElm overview)
+    {
+        Console.WriteLine($@"{Environment.NewLine}Overview:");
+        Console.WriteLine($@"ID = {overview.ID}");
+        Console.WriteLine($@"Name = {overview.Name}");
+        Console.WriteLine($@"Version = {overview.Version}");
+        Console.WriteLine($@"IsRemote = {overview.IsRemote}");
+        Console.WriteLine(
+            $@"IsDeprecated = {(overview.IsDeprecated.HasValue ? overview.IsDeprecated : ElementDoesntExist)}");
+
+        Console.WriteLine($@"Description = {overview.Description}");
+        Console.WriteLine($@"DeprecateHint = {overview.DeprecateHint}");
+    }
+
+    private static void PrintDataTypeList(ServiceInterfaceElm.DataTypeListElm dataTypeList)
+    {
+        Console.WriteLine($@"{Environment.NewLine}DataTypeList:");
+        foreach (var dataType in dataTypeList.DataTypes)
+        {
+            Console.WriteLine($@"{Environment.NewLine}New DataType:");
+            Console.WriteLine($@"ID = {dataType.ID}");
+            Console.WriteLine($@"Name = {dataType.Name}");
+            Console.WriteLine($@"Type = {dataType.Type}");
+            Console.WriteLine($@"Values = {dataType.Values}");
+            Console.WriteLine(
+                $@"IsDeprecated = {(dataType.IsDeprecated.HasValue ? dataType.IsDeprecated : ElementDoesntExist)}");
+        }
+    }
+
+    /// <summary>
+    /// This tests using the generated code from <see href="https://www.liquid-technologies.com/xml-objects"/>.
+    /// XSD "Artak_Sample_March_04.xsd" is used, as code was generated against this schema.
+    /// See <see cref="CopiedArtakSampleMarch04Validator"/>.
+    /// See code at <see href="https://www.liquid-technologies.com/Reference/XmlDataBinding/xml-objects-quick-start-guide.html#i-heading-xml-schema-validation"/>.
+    /// </summary>
+    [TestMethod]
+    [DataRow("MarksServiceTest.siml")]
+    public void TestLiquidGenCodeFor_Artak_Sample_March_04_Validation_2(string serviceFileName)
+    {
+        var xmlReaderSettings = CreateXmlReaderSettings();
+        var xmlReader = XmlReader.Create($@"{TestFilesPath}\{serviceFileName}", xmlReaderSettings);
+
+        var validator = new CopiedArtakSampleMarch04Validator();
+        using var validatingReader = validator.CreateValidatingReader(xmlReader, ValidationEventHandler);
+        var xmlDoc = new XmlDocument();
+        xmlDoc.ReadNode(validatingReader);
+    }
+
+    private static XmlReaderSettings CreateXmlReaderSettings()
+    {
         var settings = new XmlReaderSettings
         {
             ValidationType = ValidationType.Schema
@@ -46,28 +155,29 @@ public class TestSchemaValidation
         settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessSchemaLocation;
         settings.ValidationFlags |= XmlSchemaValidationFlags.ReportValidationWarnings;
 
-        var localPath = new Uri(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!).LocalPath;
-        var testFilesPath = $@"{localPath}..\..\..\..\TestFiles";
-
-        var schema = new XmlSchemaSet();
-        schema.Add("", $@"{testFilesPath}\{schemaFileName}");
-        var xmlReader = XmlReader.Create($@"{testFilesPath}\{serviceFileName}", settings);
-        
-        const LoadOptions loadOptions = LoadOptions.PreserveWhitespace | LoadOptions.SetLineInfo;
-        var xmlDocument = XDocument.Load(xmlReader, loadOptions);
-
-        xmlDocument.Validate(schema, ValidationEventHandler);
-        _numErrorsFound.Should().Be(0, "We don't want any validation errors.");
+        return settings;
     }
 
-    static void ValidationEventHandler(object? sender, ValidationEventArgs args)
+    private static void ValidationEventHandler(object? sender, ValidationEventArgs args)
     {
         _numErrorsFound++;
 
-        if (args.Severity == XmlSeverityType.Warning)
-            Console.WriteLine("Warning: Matching schema not found.  No validation occurred." + args.Message);
-        else
-            Console.WriteLine(
-                $"Validation error: [L {args.Exception.LineNumber}] [Pos {args.Exception.LinePosition}]: {args.Message}");
+        Console.WriteLine(
+            $@"Validation: [{args.Severity}] [L {args.Exception.LineNumber}] [Pos {args.Exception.LinePosition}]: {args.Message}");
+    }
+
+    /// <summary>
+    /// This is called by the Liquid XML Objects serializer.
+    /// See <see href="https://www.liquid-technologies.com/Reference/XmlDataBinding/xml-objects-code-xsd-validation.html"/>.
+    /// </summary>
+    /// <param name="msg"></param>
+    /// <param name="severity"></param>
+    /// <param name="errorCode"></param>
+    /// <param name="location"></param>
+    /// <param name="targetObject"></param>
+    private static void LxErrorHandler(string msg, LxErrorSeverity severity, LxErrorCode errorCode,
+        TextLocation? location, object targetObject)
+    {
+        Console.WriteLine($@"Liquid XML Objects Validator : {severity} : {msg}");
     }
 }
