aaeacfaa879f17e0b7943df19e4ac9ac28d89c23
Aregtech <artak@aregtech.com>
2024-09-17 00:52:46 +0200

    A try to create ui tool with Qt6 framework.

---
+++ sources/lusan/application/main/mainwindow.cpp
@@ -0,0 +1,14 @@
+#include "lusan/application/main/mainwindow.hpp"
+#include "ui/ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
