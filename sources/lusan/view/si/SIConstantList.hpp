e9e8c5833fc5b3eaa1014ece45a9f6b5bb16ea32
Aregtech <artak@aregtech.com>
2024-12-22 00:10:28 +0100

    add constant page

--- sources/lusan/view/si/SIConstantList.hpp
+++ sources/lusan/view/si/SIConstantList.hpp
@@ -1,6 +1,5 @@
-#ifndef LUSAN_APPLICATION_SI_SERVICEINTERFACE_HPP
-#define LUSAN_APPLICATION_SI_SERVICEINTERFACE_HPP
-
+#ifndef LUSAN_APPLICATION_SI_SICONSTANTLIST_HPP
+#define LUSAN_APPLICATION_SI_SICONSTANTLIST_HPP
 /************************************************************************
  *  This file is part of the Lusan project, an official component of the AREG SDK.
  *  Lusan is a graphical user interface (GUI) tool designed to support the development,
@@ -13,36 +12,27 @@
  *  with this distribution or contact us at info[at]aregtech.com.
  *
  *  \copyright   © 2023-2024 Aregtech UG. All rights reserved.
- *  \file        lusan/view/si/ServiceInterface.hpp
+ *  \file        lusan/view/si/SIConstantList.hpp
  *  \ingroup     Lusan - GUI Tool for AREG SDK
  *  \author      Artak Avetyan
- *  \brief       Lusan application, Service Interface main window.
+ *  \brief       Lusan application, Service Interface, Data Topic section.
  *
  ************************************************************************/
+#include <QWidget>
 
-#include "lusan/view/common/MdiChild.hpp"
-#include "lusan/view/si/SIConstant.hpp"
-#include "lusan/view/si/SIDataType.hpp"
-#include "lusan/view/si/SIDataTopic.hpp"
-#include "lusan/view/si/SIMethod.hpp"
-#include "lusan/view/si/SIOverview.hpp"
+namespace Ui {
+    class SIConstantList;
+}
 
-#include <QTabWidget>
-
-class ServiceInterface : public MdiChild
+class SIConstantList : public QWidget
 {
     Q_OBJECT
+
 public:
-    ServiceInterface(QWidget *parent = nullptr);
-    virtual ~ServiceInterface(void);
+    explicit SIConstantList(QWidget* parent = nullptr);
 
 private:
-    QTabWidget  mTabWidget;
-    SIOverview  mOverview;
-    SIDataType  mDataType;
-    SIDataTopic mDataTopic;
-    SIMethod    mMethod;
-    SIConstant  mConstant;
+    Ui::SIConstantList* ui;
 };
 
-#endif // LUSAN_APPLICATION_SI_SERVICEINTERFACEVIEW_HPP
+#endif // LUSAN_APPLICATION_SI_SICONSTANTLIST_HPP
