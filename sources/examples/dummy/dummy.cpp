9817eece7b3166ff1164f721ad7f55806f7630e8
Aregtech <artak@aregtech.com>
2025-03-04 12:21:24 +0100

    Removed example directory committed by mistake.

--- sources/examples/dummy/dummy.cpp
+++
@@ -1,5 +0,0 @@
-//! This is a dummy source code file.
-//! It makes nothing. Used to create a dummy project to call example-generate.bat script
-//! to generate service interface sources for all examples.
-
-#include "areg/base/GEGlobal.h"
