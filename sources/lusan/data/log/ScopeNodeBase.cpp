efbc9546fbdbae29355c08e46a4f75a34a339151
Aregtech <artak@aregtech.com>
2025-04-30 20:13:35 +0200

    Scope nodes.

---
+++ sources/lusan/data/log/ScopeNodeBase.cpp
@@ -0,0 +1,173 @@
+﻿/************************************************************************
+ *  This file is part of the Lusan project, an official component of the AREG SDK.
+ *  Lusan is a graphical user interface (GUI) tool designed to support the development,
+ *  debugging, and testing of applications built with the AREG Framework.
+ *
+ *  Lusan is available as free and open-source software under the MIT License,
+ *  providing essential features for developers.
+ *
+ *  For detailed licensing terms, please refer to the LICENSE.txt file included
+ *  with this distribution or contact us at info[at]aregtech.com.
+ *
+ *  \copyright   © 2023-2024 Aregtech UG. All rights reserved.
+ *  \file        lusan/data/log/ScopeNodeBase.cpp
+ *  \ingroup     Lusan - GUI Tool for AREG SDK
+ *  \author      Artak Avetyan
+ *  \brief       Lusan application, Log Scope base class.
+ *
+ ************************************************************************/
+/************************************************************************
+ * Include files.
+ ************************************************************************/
+#include "lusan/data/log/ScopeNodeBase.hpp"
+
+ScopeNodeBase::ScopeNodeBase(void)
+    : mNodeType     ( ScopeNodeBase::eNode::Invalid )
+    , mParent       ( nullptr )
+    , mPrioStates   ( static_cast<uint32_t>(NELogging::eLogPriority::PrioInvalid) )
+    , mNodeName     ( )
+{
+}
+
+ScopeNodeBase::ScopeNodeBase(ScopeNodeBase::eNode nodeType, ScopeNodeBase* parent /*= nullptr*/)
+    : mNodeType     ( nodeType )
+    , mParent       ( parent )
+    , mPrioStates   ( static_cast<uint32_t>(NELogging::eLogPriority::PrioInvalid) )
+    , mNodeName     ( )
+{
+}
+
+ScopeNodeBase::ScopeNodeBase(ScopeNodeBase::eNode nodeType, const QString& nodeName, unsigned int prio, ScopeNodeBase* parent)
+    : mNodeType     ( nodeType )
+    , mParent       ( parent )
+    , mPrioStates   ( prio )
+    , mNodeName     ( nodeName )
+{
+}
+
+ScopeNodeBase::ScopeNodeBase(const ScopeNodeBase& src)
+    : mNodeType     ( src.mNodeType )
+    , mParent       ( src.mParent )
+    , mPrioStates   ( src.mPrioStates )
+    , mNodeName     ( src.mNodeName )
+{
+}
+
+ScopeNodeBase::ScopeNodeBase(ScopeNodeBase&& src) noexcept
+    : mNodeType     ( src.mNodeType )
+    , mParent       ( src.mParent )
+    , mPrioStates   ( src.mPrioStates )
+    , mNodeName     ( std::move(src.mNodeName) )
+{
+}
+
+ScopeNodeBase& ScopeNodeBase::operator = (const ScopeNodeBase& src)
+{
+    ASSERT(mNodeType == src.mNodeType);
+    if (this != &src)
+    {
+        mPrioStates = src.mPrioStates;
+        mNodeName = src.mNodeName;
+    }
+
+    return (*this);
+}
+
+ScopeNodeBase& ScopeNodeBase::operator = (ScopeNodeBase&& src) noexcept
+{
+    ASSERT(mNodeType == src.mNodeType);
+    if (this != &src)
+    {
+        mPrioStates = src.mPrioStates;
+        mNodeName = std::move(src.mNodeName);
+    }
+
+    return (*this);
+}
+
+bool ScopeNodeBase::operator == (const ScopeNodeBase& other) const
+{
+    return (mNodeType == other.mNodeType) && (mNodeName == other.mNodeName);
+}
+
+bool ScopeNodeBase::operator != (const ScopeNodeBase& other) const
+{
+    return (mNodeType != other.mNodeType) || (mNodeName != other.mNodeName);
+}
+
+bool ScopeNodeBase::operator > (const ScopeNodeBase& other) const
+{
+    return (mNodeType == other.mNodeType ? (mNodeName > other.mNodeName) : (mNodeType > other.mNodeType));
+}
+
+bool ScopeNodeBase::operator < (const ScopeNodeBase& other) const
+{
+    return (mNodeType == other.mNodeType ? (mNodeName < other.mNodeName) : (mNodeType < other.mNodeType));
+}
+
+int ScopeNodeBase::addChildRecursive(QString& scopePath, uint32_t prio)
+{
+    ScopeNodeBase* node = addChildNode(scopePath, prio);
+    return ((node != nullptr) && (node->isValid()) ? 1 + node->addChildRecursive(scopePath, prio) : 0);
+}
+
+int ScopeNodeBase::addChildRecursive(QStringList& scopeNodes, uint32_t prio)
+{
+    ScopeNodeBase* node = addChildNode(scopeNodes, prio);
+    return ((node != nullptr) && (node->isValid()) ? 1 + node->addChildRecursive(scopeNodes, prio) : 0);
+}
+
+ScopeNodeBase* ScopeNodeBase::addChildNode(QString& scopePath, uint32_t prio)
+{
+    ScopeNodeBase* childNode = makeChildNode(scopePath, prio);
+    addChildNode(childNode);
+    return childNode;
+}
+
+ScopeNodeBase* ScopeNodeBase::addChildNode(QStringList& nodeNames, uint32_t prio)
+{
+    ScopeNodeBase* childNode = makeChildNode(nodeNames, prio);
+    addChildNode(childNode);
+    return childNode;
+}
+
+ScopeNodeBase* ScopeNodeBase::makeChildNode(QString& scopePath, uint32_t prio)
+{
+    return nullptr;
+}
+
+ScopeNodeBase* ScopeNodeBase::makeChildNode(QStringList& nodeNames, uint32_t prio)
+{
+    return nullptr;
+}
+
+void ScopeNodeBase::addChildNode(ScopeNodeBase* childNode)
+{
+}
+
+QStringList ScopeNodeBase::makeNodeNames(const QString& scopePath) const
+{
+    return scopePath.split(NELusanCommon::SCOPE_SEPRATOR);
+}
+
+QString ScopeNodeBase::makePath(void) const
+{
+    QString result(mParent != nullptr ? mParent->makePath() : getPathString());
+    if (result.isEmpty() == false)
+    {
+        result += NELusanCommon::SCOPE_SEPRATOR + getPathString();
+    }
+    else if (isRoot() == false)
+    {
+        result = getPathString();
+    }
+
+    return result;
+}
+
+QString ScopeNodeBase::getPathString(void) const
+{
+    return mNodeName;    
+}
+
+    
---
+++ sources/lusan/model/log/LogScopesModel.hpp
@@ -0,0 +1,111 @@
+#ifndef LUSAN_MODEL_LOG_LogScopesModel_HPP
+#define LUSAN_MODEL_LOG_LOGVIEWERMODEL_HPP
+/************************************************************************
+ *  This file is part of the Lusan project, an official component of the AREG SDK.
+ *  Lusan is a graphical user interface (GUI) tool designed to support the development,
+ *  debugging, and testing of applications built with the AREG Framework.
+ *
+ *  Lusan is available as free and open-source software under the MIT License,
+ *  providing essential features for developers.
+ *
+ *  For detailed licensing terms, please refer to the LICENSE.txt file included
+ *  with this distribution or contact us at info[at]aregtech.com.
+ *
+ *  \copyright   © 2023-2024 Aregtech UG. All rights reserved.
+ *  \file        lusan/model/log/LogScopesModel.hpp
+ *  \ingroup     Lusan - GUI Tool for AREG SDK
+ *  \author      Artak Avetyan
+ *  \brief       Lusan application, Log scopes model.
+ *
+ ************************************************************************/
+
+/************************************************************************
+ * Includes
+ ************************************************************************/
+#include <QAbstractItemModel>
+
+#include "areg/logging/private/ScopeNodes.hpp"
+
+class LogScopesModel : public QAbstractItemModel
+{
+    Q_OBJECT
+
+public:
+
+    /**
+     * \brief   Initializes the scope model object.
+     * @param   parent  The pointer to the parent object.
+     */
+    explicit LogScopesModel(QObject parent = nullptr);
+
+    /**
+     * \brief   Returns the index of the item in the model specified by the given row, column and parent index.
+     * \param   row     The row of the item.
+     * \param   column  The column of the item.
+     * \param   parent  The parent index.
+     * \return  The index of the item.
+     **/
+    virtual QModelIndex index(int row, int column, const QModelIndex& parent) const override;
+
+    /**
+     * \brief   Returns the parent index of the given child index.
+     * \param   child   The child index.
+     * \return  The parent index.
+     **/
+    virtual QModelIndex parent(const QModelIndex& child) const override;
+
+    /**
+     * \brief   Returns the number of rows under the given parent.
+     * \param   parent  The parent index.
+     * \return  The number of rows.
+     **/
+    virtual int rowCount(const QModelIndex& parent) const override;
+
+    /**
+     * \brief   Returns the number of columns for the children of the given parent.
+     * \param   parent  The parent index.
+     * \return  The number of columns.
+     **/
+    virtual int columnCount(const QModelIndex& parent) const override;
+
+    /**
+     * \brief   Returns the data stored under the given role for the item referred to by the index.
+     * \param   index   The index of the item.
+     * \param   role    The role for which data is requested.
+     * \return  The data for the given role and section.
+     **/
+    virtual QVariant data(const QModelIndex& index, int role) const override;
+
+    /**
+     * \brief   Returns the data for the given role and section in the header with the specified orientation.
+     * \param   section     The section of the header.
+     * \param   orientation The orientation of the header.
+     * \param   role        The role for which data is requested.
+     * \return  The data for the given role and section.
+     **/
+    virtual QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+
+    /**
+     * \brief   Fetches more data for the given parent index.
+     * \param   parent  The parent index.
+     **/
+    virtual void fetchMore(const QModelIndex& parent) override;
+
+    /**
+     * \brief   Checks if more data can be fetched for the given parent index.
+     * \param   parent  The parent index.
+     * \return  True if more data can be fetched, false otherwise.
+     **/
+    virtual bool canFetchMore(const QModelIndex& parent) const override;
+
+    /**
+     * \brief   Returns the flags for the item at the given index.
+     * \param   index   The index of the item.
+     * \return  The flags of the item.
+     **/
+    virtual Qt::ItemFlags flags(const QModelIndex& index) const override;
+    
+private:
+    QList<ScopeRoot>    mRootList;
+
+};
