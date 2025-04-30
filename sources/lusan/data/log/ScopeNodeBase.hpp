4224883a913e0b224962c0480adc1ca875d5c05b
Aregtech <artak@aregtech.com>
2025-04-30 20:13:35 +0200

    Scope nodes.

---
+++ sources/lusan/data/log/ScopeNodeBase.hpp
@@ -0,0 +1,333 @@
+﻿#ifndef LUSAN_DATA_LOG_SCOPENODEBASE_HPP
+#define LUSAN_DATA_LOG_SCOPENODEBASE_HPP
+/************************************************************************
+ *  This file is part of the Lusan project, an official component of the AREG SDK.
+ *  Lusan is a graphical user interface (GUI) tool designed to support the development,
+ *  debugging, and testing of applications built with the AREG Framework.
+ *
+ *  Lusan is available as free and open-source software under the MIT License,
+ *  providing essential features for developers.
+ *
+ *  For detailed licensing terms, please refer to the LICENSE.txt file included
+ *  with this distribution or contact us at info[at]aregtech.com.
+ *
+ *  \copyright   © 2023-2024 Aregtech UG. All rights reserved.
+ *  \file        lusan/data/log/ScopeNodeBase.hpp
+ *  \ingroup     Lusan - GUI Tool for AREG SDK
+ *  \author      Artak Avetyan
+ *  \brief       Lusan application, Log Scope base class.
+ *
+ ************************************************************************/
+/************************************************************************
+ * Include files.
+ ************************************************************************/
+#include "lusan/common/NELusanCommon.hpp"
+#include "areg/base/GEGlobal.h"
+#include "areg/logging/NELogging.hpp"
+
+#include <QList>
+#include <QString>
+
+class ScopeNodeBase
+{
+//////////////////////////////////////////////////////////////////////////
+// Internal types
+//////////////////////////////////////////////////////////////////////////
+protected:
+
+    /**
+     * \brief   The types of the node. Set when object is created and cannot be changed.
+     **/
+    enum class eNode    : unsigned char
+    {
+          Invalid   = 0 // The node is invalid,             bits: 0000 0000
+        , Leaf      = 2 // The node is root, has no parent, bits: 0000 0010
+        , Node      = 4 // The node that has children,      bits: 0000 0100
+        , Root      = 12// The node does not have children, bits: 0000 1100
+    };
+
+//////////////////////////////////////////////////////////////////////////
+// Protected constructors and destructor
+//////////////////////////////////////////////////////////////////////////
+protected:
+
+    //!< The default constructor. Creates an empty node with no priority.
+    ScopeNodeBase(void);
+
+    /**
+     * \brief   Creates and empty node with no priority.
+     *          The type depends on the passed parameter and cannot be changed later.
+     * \param   nodeType    The type of node to set when initialized. Cannot be changed.
+     **/
+    explicit ScopeNodeBase(ScopeNodeBase::eNode nodeType, ScopeNodeBase * parent = nullptr);
+
+    /**
+     * \brief   Creates a node and sets the node type, node name and priority flags.
+     *          The type of the node cannot be changed anymore.
+     * \param   nodeType    The type of the node. Cannot be changed anymore.
+     * \param   nodeName    The name of the node.
+     * \param   prio        The logging priority flags set bitwise.
+     **/
+    ScopeNodeBase(ScopeNodeBase::eNode nodeType, const QString& nodeName, unsigned int prio = static_cast<unsigned int>(NELogging::eLogPriority::PrioNotset), ScopeNodeBase* parent = nullptr);
+
+    //!< Copies, moves and destroys the node object.
+    ScopeNodeBase(const ScopeNodeBase& src);
+    ScopeNodeBase(ScopeNodeBase&& src) noexcept;
+    
+public:
+    
+    virtual ~ScopeNodeBase(void) = default;
+
+//////////////////////////////////////////////////////////////////////////
+// Operators
+//////////////////////////////////////////////////////////////////////////
+public:
+
+    /**
+     * \brief   Assignment operators
+     **/
+    ScopeNodeBase& operator = (const ScopeNodeBase& src);
+    ScopeNodeBase& operator = (ScopeNodeBase&& src) noexcept;
+
+    /**
+     * \brief   Compare operators.
+     **/
+    bool operator == (const ScopeNodeBase& other) const;
+    bool operator != (const ScopeNodeBase& other) const;
+    bool operator > (const ScopeNodeBase& other) const;
+    bool operator < (const ScopeNodeBase& other) const;
+
+
+//////////////////////////////////////////////////////////////////////////
+// Attributes and operations
+//////////////////////////////////////////////////////////////////////////
+public:
+    /**
+     * \brief   Returns node name.
+     **/
+    inline const QString& getNodeName(void) const;
+
+    /**
+     * \brief   Sets node name.
+     **/
+    inline void setNodeName(const QString newName);
+
+    /**
+     * \brief   Returns the node priority flag.
+     **/
+    inline unsigned int getPriority(void) const;
+
+    /**
+     * \brief   Sets the node priority flag.
+     **/
+    inline void setPriority(unsigned int prio);
+
+    /**
+     * \brief   Adds log priority bits.
+     **/
+    inline void addPriority(unsigned int prio);
+
+    /**
+     * \brief   Adds log priority bits.
+     **/
+    inline void removePriority(unsigned int prio);
+
+    /**
+     * \brief   Returns true if the object is the root. The root does not have a parent
+     **/
+    inline bool isRoot(void) const;
+
+    /**
+     * \brief   Returns true if the object is a node. Nodes have a parent, it may have child leafs and child nodes.
+     **/
+    inline bool isNode(void) const;
+
+    /**
+     * \brief   Returns true if the object is a leaf. Leafs have parent, but cannot have children nodes.
+     **/
+    inline bool isLeaf(void) const;
+
+    /**
+     * \brief   Returns true if the node is valid.
+     **/
+    inline bool isValid(void) const;
+
+    /**
+     * \brief   Returns true if the logging priority has debug priority bit set.
+     **/
+    inline bool hasPrioDebug(void) const;
+
+    /**
+     * \brief   Returns true if the logging priority has info priority bit set.
+     **/
+    inline bool hasPrioInfo(void) const;
+
+    /**
+     * \brief   Returns true if the logging priority has warning priority bit set.
+     **/
+    inline bool hasPrioWarning(void) const;
+
+    /**
+     * \brief   Returns true if the logging priority has error priority bit set.
+     **/
+    inline bool hasPrioError(void) const;
+
+    /**
+     * \brief   Returns true if the logging priority has fatal error priority bit set.
+     **/
+    inline bool hasPrioFatal(void) const;
+
+    /**
+     * \brief   Returns true if any logging priority bit is set.
+     **/
+    inline bool hasLogsEneabled(void) const;
+
+    /**
+     * \brief   Returns true if the logging scopes priority bit set.
+     **/
+    inline bool hasLogScopes(void) const;
+
+    inline ScopeNodeBase* getParent(void) const;
+
+    inline void setParent(ScopeNodeBase* parent);
+
+//////////////////////////////////////////////////////////////////////////
+// Overrides
+//////////////////////////////////////////////////////////////////////////
+protected:
+
+    virtual int addChildRecursive(QString& scopePath, uint32_t prio);
+
+    virtual int addChildRecursive(QStringList& nodeNames, uint32_t prio);
+
+    virtual ScopeNodeBase* addChildNode(QString& scopePath, uint32_t prio);
+
+    virtual ScopeNodeBase* addChildNode(QStringList& nodeNames, uint32_t prio);
+
+    virtual ScopeNodeBase* makeChildNode(QString& scopePath, uint32_t prio);
+
+    virtual ScopeNodeBase* makeChildNode(QStringList& nodeNames, uint32_t prio);
+
+    virtual void addChildNode(ScopeNodeBase* childNode);
+
+    virtual QStringList makeNodeNames(const QString& scopePath) const;
+
+    virtual QString makePath(void) const;
+    
+    virtual QString getPathString(void) const;
+
+//////////////////////////////////////////////////////////////////////////
+// Hidden methods
+//////////////////////////////////////////////////////////////////////////
+protected:
+
+//////////////////////////////////////////////////////////////////////////
+// Member variables
+//////////////////////////////////////////////////////////////////////////
+protected:
+    //!< The type of the node. Cannot be changed.
+    const ScopeNodeBase::eNode  mNodeType;
+    //!< The parent of the node. Can be null if the node is root.   
+    ScopeNodeBase*              mParent;
+    //!< The priority flags set bitwise.
+    unsigned int                mPrioStates;
+    //!< The name of the node.
+    QString                     mNodeName;
+};
+
+inline const QString & ScopeNodeBase::getNodeName( void ) const
+{
+    return mNodeName;
+}
+
+inline void ScopeNodeBase::setNodeName( const QString newName )
+{
+    mNodeName = newName;
+}
+
+inline unsigned int ScopeNodeBase::getPriority( void ) const
+{
+    return mPrioStates;
+}
+
+inline void ScopeNodeBase::setPriority( unsigned int prio )
+{
+    mPrioStates = prio;
+}
+
+inline void ScopeNodeBase::addPriority( unsigned int prio )
+{
+    mPrioStates |= prio;
+}
+
+inline void ScopeNodeBase::removePriority(unsigned int prio)
+{
+    mPrioStates &= ~prio;
+}
+
+inline bool ScopeNodeBase::isRoot( void ) const
+{
+    return (mNodeType == ScopeNodeBase::eNode::Root);
+}
+
+inline bool ScopeNodeBase::isNode( void ) const
+{
+    return (mNodeType == ScopeNodeBase::eNode::Node);
+}
+
+inline bool ScopeNodeBase::isLeaf( void ) const
+{
+    return (mNodeType == ScopeNodeBase::eNode::Leaf);
+}
+
+inline bool ScopeNodeBase::isValid( void ) const
+{
+    return (mNodeType != ScopeNodeBase::eNode::Invalid);
+}
+
+inline bool ScopeNodeBase::hasPrioDebug( void ) const
+{
+    return (mPrioStates & static_cast<uint32_t>(NELogging::eLogPriority::PrioDebug)) != 0;
+}
+
+inline bool ScopeNodeBase::hasPrioInfo( void ) const
+{
+    return (mPrioStates & static_cast<uint32_t>(NELogging::eLogPriority::PrioInfo)) != 0;
+}
+
+inline bool ScopeNodeBase::hasPrioWarning( void ) const
+{
+    return (mPrioStates & static_cast<uint32_t>(NELogging::eLogPriority::PrioWarning)) != 0;
+}
+
+inline bool ScopeNodeBase::hasPrioError( void ) const
+{
+    return (mPrioStates & static_cast<uint32_t>(NELogging::eLogPriority::PrioError)) != 0;
+}
+
+inline bool ScopeNodeBase::hasPrioFatal( void ) const
+{
+    return (mPrioStates & static_cast<uint32_t>(NELogging::eLogPriority::PrioFatal)) != 0;
+}
+
+inline bool ScopeNodeBase::hasLogsEneabled( void ) const
+{
+    return (mPrioStates & static_cast<uint32_t>(NELogging::eLogPriority::PrioLogs)) != 0;
+}
+
+inline bool ScopeNodeBase::hasLogScopes( void ) const
+{
+    return (mPrioStates & static_cast<uint32_t>(NELogging::eLogPriority::PrioScope)) != 0;
+}
+
+inline ScopeNodeBase* ScopeNodeBase::getParent(void) const
+{
+    return mParent;
+}
+
+void ScopeNodeBase::setParent(ScopeNodeBase* parent)
+{
+    mParent = parent;
+}
+
+#endif  // LUSAN_DATA_LOG_SCOPENODEBASE_HPP
---
+++ sources/lusan/data/log/ScopeNodes.hpp
@@ -0,0 +1,315 @@
+﻿#ifndef LUSAN_DATA_LOG_SCOPENODES_HPP
+#define LUSAN_DATA_LOG_SCOPENODES_HPP
+/************************************************************************
+ *  This file is part of the Lusan project, an official component of the AREG SDK.
+ *  Lusan is a graphical user interface (GUI) tool designed to support the development,
+ *  debugging, and testing of applications built with the AREG Framework.
+ *
+ *  Lusan is available as free and open-source software under the MIT License,
+ *  providing essential features for developers.
+ *
+ *  For detailed licensing terms, please refer to the LICENSE.txt file included
+ *  with this distribution or contact us at info[at]aregtech.com.
+ *
+ *  \copyright   © 2023-2024 Aregtech UG. All rights reserved.
+ *  \file        lusan/data/log/ScopeNodes.hpp
+ *  \ingroup     Lusan - GUI Tool for AREG SDK
+ *  \author      Artak Avetyan
+ *  \brief       Lusan application, Log Scope node classes.
+ *
+ ************************************************************************/
+/************************************************************************
+ * Include files.
+ ************************************************************************/
+#include "lusan/data/log/ScopeNodeBase.hpp"
+#include "areg/base/TESortedLinkedList.hpp"
+
+#include <QMap>
+
+class ScopeLeaf;
+class ScopeNode;
+class ScopeRoot;
+
+//////////////////////////////////////////////////////////////////////////
+// ScopeLeaf class declaration
+//////////////////////////////////////////////////////////////////////////
+/**
+ * \brief   The Leaf node, which has a parent, but does not have children nodes.
+ **/
+class ScopeLeaf : public ScopeNodeBase
+{
+//////////////////////////////////////////////////////////////////////////
+// Constructors / destructor
+//////////////////////////////////////////////////////////////////////////
+public:
+    ScopeLeaf( ScopeNode * parent = nullptr );
+
+    ScopeLeaf(const QString leafName, uint32_t prio = static_cast<unsigned int>(NELogging::eLogPriority::PrioNotset), ScopeNode* parent = nullptr);
+
+    /**
+     * \brief   Copies data from the ScopeNodeBase object. Should be called explicit.
+     **/
+    explicit ScopeLeaf( const ScopeNodeBase & base );
+
+    /**
+     * \brief   Copies or moves data from the given source
+     **/
+    ScopeLeaf( const ScopeLeaf & src );    
+    ScopeLeaf( ScopeLeaf && src ) noexcept;
+
+    virtual ~ScopeLeaf( void ) = default;
+
+//////////////////////////////////////////////////////////////////////////
+// Operators
+//////////////////////////////////////////////////////////////////////////
+public:
+
+    /**
+     * \brief   Assigns data from the given source. Default implementation
+     **/
+    ScopeLeaf & operator = ( const ScopeLeaf & src ) = default;
+    ScopeLeaf & operator = ( ScopeLeaf && src ) = default;
+
+//////////////////////////////////////////////////////////////////////////
+// Overrides
+//////////////////////////////////////////////////////////////////////////
+public:
+/************************************************************************
+ * ScopeNodeBase override
+ ************************************************************************/
+
+    virtual ScopeNodeBase* makeChildNode(QString& scopePath, uint32_t prio) override;
+
+    virtual ScopeNodeBase* makeChildNode(QStringList& nodeNames, uint32_t prio) override;
+
+    virtual void addChildNode(ScopeNodeBase* childNode) override;
+};
+
+//////////////////////////////////////////////////////////////////////////
+// ScopeNode class declaration
+//////////////////////////////////////////////////////////////////////////
+/**
+ * \brief   The scope node, which has parent, leafs and child nodes.
+ **/
+class ScopeNode : public ScopeNodeBase
+{
+//////////////////////////////////////////////////////////////////////////
+// Internal types
+//////////////////////////////////////////////////////////////////////////
+public:
+    using NodeList = QMap<QString, ScopeNode *>;
+    using LeafList = QMap<QString, ScopeLeaf *>;
+
+//////////////////////////////////////////////////////////////////////////
+// Constructors / Destructor
+//////////////////////////////////////////////////////////////////////////
+public:
+    ScopeNode( ScopeNode * parent = nullptr );
+
+    ScopeNode(const QString nodeName, uint32_t prio = static_cast<unsigned int>(NELogging::eLogPriority::PrioNotset), ScopeNodeBase* parent = nullptr);
+
+    /**
+     * \brief   Creates a node with empty list of child leafs and nodes
+     *          and copies data from the base object.
+     **/
+    explicit ScopeNode( const ScopeNodeBase & base );
+
+    /**
+     * \brief   Copies or moves data from the given source
+     **/
+    ScopeNode( const ScopeNode & src );    
+    ScopeNode( ScopeNode && src ) noexcept;
+    
+    virtual ~ScopeNode( void );
+
+//////////////////////////////////////////////////////////////////////////
+// Protected constructor
+//////////////////////////////////////////////////////////////////////////
+protected:
+    /**
+     * \brief   Protected constructor required by root node.
+     **/
+    ScopeNode( ScopeNodeBase::eNode nodeType, const QString & name, unsigned int prio );
+
+//////////////////////////////////////////////////////////////////////////
+// Assigning operators
+//////////////////////////////////////////////////////////////////////////
+public:
+    /**
+     * \brief   Copies data from the given source.
+     **/
+    ScopeNode & operator = ( const ScopeNode & src );
+    ScopeNode & operator = ( ScopeNode && src ) noexcept;
+
+//////////////////////////////////////////////////////////////////////////
+// Override
+//////////////////////////////////////////////////////////////////////////
+public:
+
+/************************************************************************
+ * ScopeNodeBase override
+ ************************************************************************/
+
+    virtual ScopeNodeBase* makeChildNode(QString& scopePath, uint32_t prio) override;
+
+    virtual ScopeNodeBase* makeChildNode(QStringList& nodeNames, uint32_t prio) override;
+
+    virtual void addChildNode(ScopeNodeBase* childNode) override;
+
+//////////////////////////////////////////////////////////////////////////
+// Attributes and operations
+//////////////////////////////////////////////////////////////////////////
+public:
+    /**
+     * \brief   Returns the list of child nodes.
+     **/
+    inline const ScopeNode::NodeList & getNodes( void ) const;
+
+    /**
+     * \brief   Returns the list of child leafs.
+     **/
+    inline const ScopeNode::LeafList & getLeafs( void ) const;
+
+    /**
+     * \brief   Returns the total number of children.
+     **/
+    inline unsigned int childNodeCount( void ) const;
+
+//////////////////////////////////////////////////////////////////////////
+// Protected members
+//////////////////////////////////////////////////////////////////////////
+protected:
+    //!< The list of child nodes.
+    NodeList    mChildNodes;
+    //!< The list of child leafs.
+    LeafList    mChildLeafs;
+};
+
+//////////////////////////////////////////////////////////////////////////
+// ScopeRoot class declaration
+//////////////////////////////////////////////////////////////////////////
+/**
+ * \brief   The scope node, which has parent, leafs and child nodes.
+ **/
+class ScopeRoot : public ScopeNode
+{
+//////////////////////////////////////////////////////////////////////////
+// Constructors / Destructor
+//////////////////////////////////////////////////////////////////////////
+public:
+    ScopeRoot(void);
+
+    explicit ScopeRoot( ITEM_ID rootId );
+
+    ScopeRoot(ITEM_ID rootId, const QString rootName );
+
+    /**
+     * \brief   Copies or moves data from the given source
+     **/
+    ScopeRoot( const ScopeRoot & src );
+    ScopeRoot( ScopeRoot && src ) noexcept;
+    
+    virtual ~ScopeRoot( void ) = default;
+
+//////////////////////////////////////////////////////////////////////////
+// Assigning operators
+//////////////////////////////////////////////////////////////////////////
+public:
+    /**
+     * \brief   Copies data from the given source.
+     **/
+    ScopeRoot & operator = ( const ScopeRoot & src );
+    ScopeRoot & operator = ( ScopeRoot && src ) noexcept;
+
+//////////////////////////////////////////////////////////////////////////
+// Override
+//////////////////////////////////////////////////////////////////////////
+public:
+
+/************************************************************************
+ * ScopeNodeBase override
+ ************************************************************************/
+    
+    virtual int addChildRecursive(QString& scopePath, uint32_t prio);
+    
+    virtual int addChildRecursive(QStringList& nodeNames, uint32_t prio);
+    
+    virtual QString getPathString(void) const;
+
+//////////////////////////////////////////////////////////////////////////
+// Attributes and operations
+//////////////////////////////////////////////////////////////////////////
+public:
+    /**
+     * \brief   Returns the ID of the root.
+     **/
+    inline ITEM_ID getNodeId( void ) const;
+
+    /**
+     * \brief   Sets root ID
+     **/
+    inline void setNodeId(const ITEM_ID rootId);
+
+    /**
+     * \brief   Returns the root node name.
+     **/
+    inline const QString & getRootName(void) const;
+
+    /**
+     * \brief   Sets root node name
+     * \param   newRoot     New name of the root to set.
+     **/
+    inline void setRootName(const QString& newRoot);
+
+//////////////////////////////////////////////////////////////////////////
+// Protected members
+//////////////////////////////////////////////////////////////////////////
+private:
+    //!< The ID of the root.
+    ITEM_ID     mRootId;
+};
+
+//////////////////////////////////////////////////////////////////////////
+// ScopeNode class inline methods
+//////////////////////////////////////////////////////////////////////////
+
+inline const ScopeNode::NodeList& ScopeNode::getNodes(void) const
+{
+    return mChildNodes;
+}
+
+inline const ScopeNode::LeafList& ScopeNode::getLeafs(void) const
+{
+    return mChildLeafs;
+}
+
+inline unsigned int ScopeNode::childNodeCount(void) const
+{
+    return (mChildLeafs.size() + mChildNodes.size());
+}
+
+//////////////////////////////////////////////////////////////////////////
+// ScopeRoot class inline methods
+//////////////////////////////////////////////////////////////////////////
+
+inline ITEM_ID ScopeRoot::getNodeId(void) const
+{
+    return mRootId;
+}
+
+inline void ScopeRoot::setNodeId(const ITEM_ID rootId)
+{
+    mRootId = rootId;
+}
+
+inline const QString& ScopeRoot::getRootName(void) const
+{
+    return mNodeName;
+}
+
+inline void ScopeRoot::setRootName(const QString& newRoot)
+{
+    mNodeName = newRoot;
+}
+
+#endif  // LUSAN_DATA_LOG_SCOPENODES_HPP
